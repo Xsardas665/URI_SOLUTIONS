e904273025e76187081e041054d9e08d7ed3d97a
Maciej Zygadło <Xsardas665@gmail.com>
2020-10-29 20:05:20 +0100

    Added 1010.cpp to Beginner Set folder. Added 1011.cpp to Beginner Set folder. Added 1012.cpp to Beginner Set folder.

---
+++ Beginner Set/1010.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int c1,c2,u1,u2;
+  	float p1,p2;
+  	scanf("%d %d %f",&c1,&u1,&p1);
+  	scanf("%d %d %f",&c2,&u2,&p2);
+  	printf("VALOR A PAGAR: R$ %.2f\n",(u1*p1)+(u2*p2));
+    return 0;
+}
