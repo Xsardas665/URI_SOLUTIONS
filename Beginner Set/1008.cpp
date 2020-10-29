30dd583baadff3c67001c55820257c07d02f9fb9
Maciej Zygadło <Xsardas665@gmail.com>
2020-10-29 20:05:03 +0100

    Added 1007.cpp to Beginner Set folder. Added 1008.cpp to Beginner Set folder. Added 1009.cpp to Beginner Set folder.

---
+++ Beginner Set/1008.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number,wh;
+    float pph,salary;
+    scanf("%d%d%f", &number, &wh, &pph);
+    salary = wh*pph;
+    printf("NUMBER = %d\nSALARY = U$ %.2f\n", number, salary);
+    return 0;
+}
---
+++ Beginner Set/1009.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char name[100];
+    double ms,ts,total;
+    scanf("%[^\n]", &name);
+    scanf("%lf%lf", &ms, &ts);
+    total = ms+(ts*0.15);
+    printf("TOTAL = R$ %.2f\n", total);
+    return 0;
+}
