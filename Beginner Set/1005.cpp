e48dfc274bbed662ca14b445b872d520dd24dd2d
Maciej Zygadło <Xsardas665@gmail.com>
2020-10-29 20:02:11 +0100

    Added 1005.cpp to Beginner Set folder.

---
+++ Beginner Set/1005.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double A,B,MEDIA;
+    float C=3.5,D=7.5;
+    scanf("%lf%lf", &A, &B);
+    MEDIA = ((A*C)+(B*D))/(C+D);
+    printf("MEDIA = %.5lf\n", MEDIA);
+    return 0;
+}
